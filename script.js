832e841aef95e9c64c94d0d47a79150f3ae7646b
TheoDerive <theo.derive.pro@gmail.com>
2024-10-01 13:39:26 +0000

    Perfect nav

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function toggleNav() {
 
 navClose.addEventListener("click", toggleNav);
 navOpen.addEventListener("click", toggleNav);
+
+const navContainer = document.querySelector(".desktop-nav");
+const firstOnglet = document.querySelector(".first");
+const secondOnglet = document.querySelector(".second");
+const thirdOnglet = document.querySelector(".third");
+const fourthOnglet = document.querySelector(".fourth");
+
+function openNavBackground() {
+  navContainer.style.background = `rgba(255, 255, 255, 1)`;
+}
+
+function closeNavBackground() {
+  navContainer.style.background = `rgba(255, 255, 255, .7)`;
+}
+
+firstOnglet.addEventListener("mouseenter", openNavBackground);
+firstOnglet.addEventListener("mouseleave", closeNavBackground);
+secondOnglet.addEventListener("mouseenter", openNavBackground);
+secondOnglet.addEventListener("mouseleave", closeNavBackground);
+thirdOnglet.addEventListener("mouseenter", openNavBackground);
+thirdOnglet.addEventListener("mouseleave", closeNavBackground);
+fourthOnglet.addEventListener("mouseenter", openNavBackground);
+fourthOnglet.addEventListener("mouseleave", closeNavBackground);
